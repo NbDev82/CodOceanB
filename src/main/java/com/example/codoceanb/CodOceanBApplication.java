74ac9d69e0389ae93ab9d7ee61dbc4e52eff391b
Van Hoang <21110828@student.hcmute.edu.vn>
2024-06-18 22:53:14 +0700

    first commit

---
+++ src/main/java/com/example/codoceanb/CodOceanBApplication.java
@@ -0,0 +1,13 @@
+package com.example.codoceanb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodOceanBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodOceanBApplication.class, args);
+	}
+
+}
